58102dcd0b7b447d193afc42a4780217329eca78
zhongxiang <fromzhongxiang@163.com>
2018-11-29 17:42:23 +0800

    123

--- app/Http/Controllers/admin/AdController.php
+++ app/Http/Controllers/admin/AdController.php
@@ -42,7 +42,7 @@ class AdController extends Controller
     {
         $file = 'http://video.yidianzixun.com/video/get-url?key=user_upload/1543458185676462743949424c54b6a4798c1e301da39.mp4';
         $vtime = exec("ffmpeg -i ".$file." 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//");//总长度
-        $ctime = date("Y-m-d H:i:s",filectime($file));//创建时间
+//        $ctime = date("Y-m-d H:i:s",filectime($file));//创建时间
         //$duration = explode(":",$time);
         // $duration_in_seconds = $duration[0]*3600 + $duration[1]*60+ round($duration[2]);//转化为秒
 //        return array('vtime'=>$vtime,
