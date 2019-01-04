bf152331c50a09ce4016e874317144422ce3410e
zhongxiang <fromzhongxiang@163.com>
2019-01-04 14:42:03 +0800

    123

--- routes/api.php
+++ routes/api.php
@@ -13,22 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 Route::group(['namespace' => 'api', 'middleware' => ['api']], function ($router) {
-    $router->get('test',function(){
-        echo 1111111111222222;
+    $router->get('wxLogin', 'ApiTemplateController@wxLogin');
+    $router->get('tasks', 'ApiTemplateController@tasks');
+    $router->any('detail', 'ApiTemplateController@detail');
+    $router->any('getLogData', 'ApiTemplateController@getLogData');
+    $router->get('profit', 'ApiTemplateController@profit');
+    $router->get('setting', 'ApiTemplateController@setting');
+    $router->get('operateTask', 'ApiTemplateController@operateTask');
+    $router->get('userInfo', 'ApiTemplateController@userInfo');
+    $router->any('saveMobile','ApiTemplateController@saveMobile');
+    $router->any('login',function(){
+        return view('api.login');
     });
-//    $router->get('wxLogin', 'ApiTemplateController@wxLogin');
-//    $router->get('tasks', 'ApiTemplateController@tasks');
-//    $router->any('detail', 'ApiTemplateController@detail');
-//    $router->any('getLogData', 'ApiTemplateController@getLogData');
-//    $router->get('profit', 'ApiTemplateController@profit');
-//    $router->get('setting', 'ApiTemplateController@setting');
-//    $router->get('operateTask', 'ApiTemplateController@operateTask');
-//    $router->get('userInfo', 'ApiTemplateController@userInfo');
-//    $router->any('saveMobile','ApiTemplateController@saveMobile');
-//    $router->any('login',function(){
-//        return view('api.login');
-//    });
-//
-//    //微信通知信息
-//    $router->any('message', 'WechatApiController@message');
+
+    //微信通知信息
+    $router->any('message', 'WechatApiController@message');
 });
