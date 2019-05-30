7f11539d55d48c51c14b60f9d745798721bb0775
zhongxiang <fromzhongxiang@163.com>
2019-05-30 14:29:35 +0800

    123

--- app/Repositories/api/ImageRepository.php
+++ app/Repositories/api/ImageRepository.php
@@ -35,9 +35,9 @@ class ImageRepository
        $list = $this->_image->select(['id','picUrl','views','favors','type'])->limit($limit)->offset($offset)->orderby('views','desc')->get();
        if(!empty($list)){
            $list = $list->toArray();
-           return $list;
+           return message(200,$list);
        }else{
-           return [];
+           return message(-200);
        }
 
 
