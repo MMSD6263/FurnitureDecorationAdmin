df5bbc83e49dd473974ca2ac18e2f2af0d4a9606
zhongxiang <fromzhongxiang@163.com>
2019-05-30 14:01:59 +0800

    123

--- app/Repositories/api/ImageRepository.php
+++ app/Repositories/api/ImageRepository.php
@@ -17,22 +17,22 @@ class ImageRepository
    {
        $offset = $request['page'];
        $limit  = 10;
-       if(isset($request['type_id'])){
-           $this->_image = whereserach::whereid($this->_image,'type_id',$request['type_id']);
-       }
-        if(isset($reqeust['style_id'])){
-            $this->_image = whereserach::whereid($this->_image,'style_id',$request['style_id']);
-        }
-       if(isset($reqeust['style_id'])){
-           $this->_image = whereserach::whereid($this->_image,'style_id',$request['style_id']);
-       }
-       if(isset($reqeust['color_id'])){
-           $this->_image = whereserach::whereid($this->_image,'color_id',$request['color_id']);
-       }
-       if(isset($reqeust['room_id'])){
-           $this->_image = whereserach::whereid($this->_image,'style_id',$request['room_id']);
-       }
-       $list = $this->_image->select(['id','picUrl','views','favors','type_id'])->limit($limit)->offset($offset)->orderby('views','desc')->get();
+//       if(isset($request['type_id'])){
+//           $this->_image = whereserach::whereid($this->_image,'type_id',$request['type_id']);
+//       }
+//        if(isset($reqeust['style_id'])){
+//            $this->_image = whereserach::whereid($this->_image,'style_id',$request['style_id']);
+//        }
+//       if(isset($reqeust['style_id'])){
+//           $this->_image = whereserach::whereid($this->_image,'style_id',$request['style_id']);
+//       }
+//       if(isset($reqeust['color_id'])){
+//           $this->_image = whereserach::whereid($this->_image,'color_id',$request['color_id']);
+//       }
+//       if(isset($reqeust['room_id'])){
+//           $this->_image = whereserach::whereid($this->_image,'style_id',$request['room_id']);
+//       }
+       $list = $this->_image->select(['id','picUrl','views','favors','type'])->limit($limit)->offset($offset)->orderby('views','desc')->get();
        if(!empty($list)){
            $list = $list->toArray();
            return $list;
