25cced0e0e9717f57615c8efd7bab5f4dc5ea3b2
zhongxiang <fromzhongxiang@163.com>
2019-05-30 18:02:39 +0800

    12

--- app/Repositories/api/ImageRepository.php
+++ app/Repositories/api/ImageRepository.php
@@ -43,10 +43,9 @@ class ImageRepository
            $arr = [];
            foreach($list as &$value){
                $value['thumbCover'] = getenv('DOMAIN').json_decode($value['picUrl'],true)[0];
-               $value['picUrl'] = json_decode($value['picUrl'],true);
-               $arr[$value['id']] = $value;
+               $arr[$value['id']] = json_decode($value['picUrl'],true);
            }
-           return ['lists'=>$arr,'total'=>count($arr)];
+           return ['lists'=>$list,'picList'=>$arr,'total'=>count($arr)];
        }else{
            return [];
        }
