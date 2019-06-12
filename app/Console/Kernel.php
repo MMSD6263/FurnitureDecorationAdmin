1a05ea67f5a6dbae493a4e1b1c1a48c5d5f125c1
zhongxiang <fromzhongxiang@163.com>
2019-06-12 15:08:06 +0800

    123

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(ArticleAuditorConsole::class)->cron('10/* * * * *');
+        $schedule->command(ArticleAuditorConsole::class)->cron('*/10 * * * *');
     }
 
     /**
